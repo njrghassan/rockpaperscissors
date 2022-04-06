7ef452a423127d576011c0c0c85ab9865a956283
Ghassan AlNajjar <63667808+AppleGamer77@users.noreply.github.com>
2022-04-06 23:42:24 +0300

    sii

--- rps/java.js
+++ rps/java.js
@@ -7,84 +7,10 @@ const result_div = document.querySelector(".result > h3");
 const rock_div = document.getElementById("Rock");
 const paper_div = document.getElementById("Paper");
 const scissors_div = document.getElementById("Scissors");
-const rockPc_div = document.getElementById("rockPc");
-const paperPc_div = document.getElementById("paperPc");
-const scissorsPc_div = document.getElementById("scissorsPc");
-
 
 //console log things
 console.log(document.all)
-alert("The game still in devlopment but have fun :D ")
 
-// The computer's Choice
-function getPcChoice(){
-    const choices = ['Rock', 'Paper', 'Scissors']
-    const randomNumber = Math.floor(Math.random() * 3);
-    return choices[randomNumber];
-}
-// wining ond lossing XD
-function win(userChoice, pcChoice){
-    userScore++;
-    if(userScore > 5 ){
-    userScore = 0;
-    pcScore = 0;
-    alert("You WIN !");
-    location.reload()
-    }
-    userScore_span.innerHTML = userScore;
-    pcScore_span.innerHTML = pcScore;
-    const player = "Player".fontsize(4).fontcolor("#dc3545").sup();
-    const PC = "PC".fontsize(4).fontcolor("#dc3545").sup();
-    const win = "Win!".fontcolor("#28a745")
-    result_div.innerHTML = userChoice + player + " Beats "+ pcChoice + PC +" "+ " You "+ win;
-}
-function loses(userChoice, pcChoice){
-    pcScore++;
-    if(pcScore > 5){
-    pcScore = 0;
-    userScore = 0;
-    alert("You lost :(");
-    location.reload()
-    }
-    pcScore_span.innerHTML = pcScore;
-    userScore_span.innerHTML = userScore;
-    const player = "Player".fontsize(4).fontcolor("#dc3545").sup();
-    const PC = "PC".fontsize(4).fontcolor("#dc3545").sup();
-    const lose = "lose!".fontcolor("#d42424") 
-    result_div.innerHTML = pcChoice +PC + " Beats " + userChoice+ player + " You " + lose;
-    
-}
-function draw(userChoice, pcChoice){
-    const player = "Player".fontsize(4).fontcolor("#dc3545").sup();
-    const PC = "PC".fontsize(4).fontcolor("#dc3545").sup();
-    const draw = "draw!".fontcolor("#6c757d")
-    result_div.innerHTML = pcChoice + PC + " " + userChoice + player + " It's a " + draw;
-    
-}
-
-// wining case's
-function game(userChoice){
-    const pcChoice = getPcChoice();
-    var name =userChoice + pcChoice
-    switch (userChoice + pcChoice){
-        case "RockScissors":
-        case "PaperRock":    
-        case "ScissorsPaper":
-            win(userChoice , pcChoice);
-              break;
-        case "RockPaper":
-        case "PaperScissors":
-        case "ScissorsRock":
-            loses(userChoice , pcChoice);
-                break;
-        case "RockRock":
-        case "PaperPaper":
-        case "ScissorsScissors":
-            draw(userChoice , pcChoice);            
-                break;
-    }
-    console.log("Pc --->"+ " " + pcChoice);  
-}
 
 // Button's which is for player's are thiiiiccccccccc
 function main(){
@@ -103,3 +29,102 @@ function main(){
 
 main();
 
+// The computer's Choice
+function getPcChoice(){
+    const choices = ['Rock', 'Paper', 'Scissors']
+    const randomNumber = Math.floor(Math.random() * 3);
+    return choices[randomNumber];
+}
+
+// wining case's
+function game(userChoice){
+    const pcChoice = getPcChoice();
+    var name = userChoice + pcChoice
+    switch (userChoice + pcChoice){
+        case "RockScissors":
+        case "PaperRock":    
+        case "ScissorsPaper":
+            win(userChoice , pcChoice);
+              break;
+        case "RockPaper":
+        case "PaperScissors":
+        case "ScissorsRock":
+            loses(userChoice , pcChoice);
+                break;
+        case "RockRock":
+        case "PaperPaper":
+        case "ScissorsScissors":
+            draw(userChoice , pcChoice);            
+                break;
+    }
+}
+
+// wining ond lossing XD
+function win(userChoice, pcChoice){
+    userScore++;
+    if(userScore > 4){
+    alert("You WIN !");
+    document.getElementById("Rock").disabled = true;
+    document.getElementById("Paper").disabled = true;
+    document.getElementById("Scissors").disabled = true;
+    var buttonComeHere = x
+    if (buttonComeHere.style.display === "block") {
+        buttonComeHere.style.display = "none";
+    }else {
+        buttonComeHere.style.display = "block";
+    }
+    buttonComeHere.style.display = "inline";
+    }
+    userScore_span.innerHTML = userScore;
+    pcScore_span.innerHTML = pcScore;
+    const player = "Player".fontsize(4).fontcolor("#dc3545").sup();
+    const PC = "PC".fontsize(4).fontcolor("#dc3545").sup();
+    const win = "Win!".fontcolor("#28a745")
+    result_div.innerHTML = userChoice + player + " Beats "+ pcChoice + PC +" "+ " You "+ win;  
+}
+function loses(userChoice, pcChoice){
+    pcScore++;
+    if(pcScore > 4){
+    alert("You lost :(");
+    document.getElementById("Rock").disabled = true;
+    document.getElementById("Paper").disabled = true;
+    document.getElementById("Scissors").disabled = true;
+    var buttonComeHere = x
+    if (buttonComeHere.style.display === "block") {
+        buttonComeHere.style.display = "none";
+    }else {
+        buttonComeHere.style.display = "block";
+    }
+    buttonComeHere.style.display = "inline";
+    }
+    pcScore_span.innerHTML = pcScore;
+    userScore_span.innerHTML = userScore;
+    const player = "Player".fontsize(4).fontcolor("#dc3545").sup();
+    const PC = "PC".fontsize(4).fontcolor("#dc3545").sup();
+    const lose = "lose!".fontcolor("#d42424") 
+    result_div.innerHTML = pcChoice +PC + " Beats " + userChoice+ player + " You " + lose;
+    console.log("pc"+pcScore)
+}
+function draw(userChoice, pcChoice){
+    const player = "Player".fontsize(4).fontcolor("#dc3545").sup();
+    const PC = "PC".fontsize(4).fontcolor("#dc3545").sup();
+    const draw = "draw!".fontcolor("#6c757d")
+    result_div.innerHTML = pcChoice + PC + " " + userChoice + player + " It's a " + draw;  
+}
+
+//Play again function
+function apperAgain(){
+    pcScore = 0;
+    userScore = 0;
+    document.getElementById("user-score").innerText = 0;
+    document.getElementById("pc-score").innerText = 0;
+    document.getElementById("resultAgain").innerText = "Choose to get started !";
+    document.getElementById("Rock").disabled = false;
+    document.getElementById("Paper").disabled = false;
+    document.getElementById("Scissors").disabled = false;
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    }else {
+        x.style.display = "none";
+    }
+}
